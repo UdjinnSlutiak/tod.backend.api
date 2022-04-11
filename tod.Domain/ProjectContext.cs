880db6101ddd5b248e942f25b162edd5e4e46eb7
Eugene Slutiak <72391377+UdjinnSlutiak@users.noreply.github.com>
2022-04-11 11:58:19 +0300

    Initialized all projects, made some configs, added necessary folders

---
+++ tod.Domain/ProjectContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace tod.Domain
+{
+	public class ProjectContext : DbContext
+	{
+		public ProjectContext(DbContextOptions<ProjectContext> options)
+			: base(options)
+		{
+			Database.EnsureCreated();
+		}
+	}
+}
---
+++ tod.Web/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using tod.Domain;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Adding all necessary services into web project
+
+var services = builder.Services;
+
+services.AddControllers();
+
+services.AddDbContext<ProjectContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+
+// services.AddSwaggerGen();
+
+// Building web application and adding all necessary middlewares
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
