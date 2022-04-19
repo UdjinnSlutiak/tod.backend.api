00ceddc19fbc3d7c5846a3df7aa267bf7a02fe44
Slutiak Eugene <slutiak.jenia@gmail.com>
2022-04-20 00:58:11 +0300

    Added Jwt and its configurations

---
+++ tod.Services/Jwt/JwtTokenConfigurator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace Tod.Services.Jwt
+{
+	public static class JwtTokenConfigurator
+	{
+		public static IServiceCollection AddJwtTokenConfiguration(this IServiceCollection services, out JwtTokenConfiguration jwtConfiguration)
+        {
+			using (var reader = new StreamReader("JwtTokenConfig.json"))
+            {
+				var configJson = reader.ReadToEndAsync().Result;
+				var config = JsonConvert.DeserializeObject<JwtTokenConfiguration>(configJson);
+
+				jwtConfiguration = new JwtTokenConfiguration()
+				{
+					Secret = config.Secret,
+					Issuer = config.Issuer,
+					Audience = config.Audience,
+					AccessTokenExpiresInMinutes = config.AccessTokenExpiresInMinutes,
+					RefreshTokenExpiresInMinutes = config.RefreshTokenExpiresInMinutes
+				};
+
+				services.AddSingleton(jwtConfiguration);
+
+				return services;
+			}
+        }
+	}
+}
